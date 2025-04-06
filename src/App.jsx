33cf879c8974cc2f0e6388b61f39327463c18eb0
Marko Matic <maticmarko08@gmail.comgit config --global user.email ,maticmarko08@gmail.com>
2025-04-06 11:24:20 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
 
 let name="Marko"
+let paragraphColor="magenta"
 
 function App() {
   
 
   return (
     <>
-    <p>
-      My name is {name}
-      </p>
+    <p>My name is {name}</p>
+      
+
+      <p style={{color:paragraphColor}}>Ovaj tekst ce biti {paragraphColor} boje</p>
       </>
   )
 }
+
+
+
 
 export default App
